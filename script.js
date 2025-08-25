a55896328511effc97c521f34b6c3efa48a7dcc2
unknown <jainpallavi.delhi@gmail.com>
2025-08-26 00:22:21 +0530

    finalized

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Funny distracting messages
 const messages = [
   "🚨 Stop working! Check if your fridge light turns off when the door closes.",
   "😜 Hey! Scroll Instagram, someone might have posted a cat meme!",
@@ -7,12 +6,12 @@ const messages = [
   "😂 Imagine if you were working instead of reading this…"
 ];
 
-// Create alarm to trigger every 10 minutes
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.alarms.create("reminder", { delayInMinutes: 1, periodInMinutes: 10 });
 });
 
-// Show notification when alarm fires
+
 chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === "reminder") {
     const message = messages[Math.floor(Math.random() * messages.length)];
@@ -23,7 +22,7 @@ chrome.alarms.onAlarm.addListener((alarm) => {
       message: message,
       priority: 2
     }, (id) => {
-      // Auto clear notification after 10 seconds
+      
       setTimeout(() => {
         chrome.notifications.clear(id);
       }, 10000);
